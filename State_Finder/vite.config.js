4e754bb80cfb4570a0640cff93e30cd4d9702202
NadilaBasilu <basilunw@gmail.com>
2025-12-30 15:40:40 +0530

    Add dependancies for the deployemnt

--- State_Finder/vite.config.js
+++ State_Finder/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/State-Finder-App/",
     test: {
         globals: true,
         environment: 'happy-dom',
